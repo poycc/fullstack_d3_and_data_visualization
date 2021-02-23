69811b883b06d353de678228a30d899f4ed4969d
zhued <zhufind@yeah.net>
2021-02-23 17:37:46 +0800

    fix(path): fixed

--- 04-animations-and-transitions/draw-bars-with-css-transition/js/index.js
+++ 04-animations-and-transitions/draw-bars-with-css-transition/js/index.js
@@ -1,6 +1,6 @@
 async function drawBars() {
   // 1. Access data
-  const dataset = await d3.json("../../../data/my_weather_data.json");
+  const dataset = await d3.json("../../data/my_weather_data.json");
 
   // 2. Create dimensions
   const width = 500;
